88c764c5916c4d5cff9e60fde773176091efd917
Rafael Penna <rgarciapenna@gmail.com>
2023-07-25 19:03:39 -0300

    Project from lesson HikeApp created.

---
+++ HikeApp/HikeApp/Component/CardView.swift
@@ -0,0 +1,92 @@
+//
+//  CardView.swift
+//  HikeApp
+//
+//  Created by Rafael Penna on 25/07/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    @State private var isShowingsheet: Bool = false
+    
+    func randomImage() {
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        imageNumber = randomNumber
+    }
+    
+    var body: some View {
+        ZStack {
+            CustomBackgroundView()
+            
+            VStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        
+                        Spacer()
+                        
+                        Button {
+                            isShowingsheet.toggle()
+                        } label: {
+                            CustomButtonView()
+                        }
+                        .sheet(isPresented: $isShowingsheet) {
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
+                        }
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                    
+                }
+                
+                .padding(.horizontal, 30)
+                
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-\(imageNumber)")
+                        .resizable()
+                        .scaledToFit()
+                        .animation(.default, value: imageNumber)
+                }
+                
+                Button {
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(LinearGradient(colors: [.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom))
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
+                
+            }
+        }
+        .frame(width: 320, height: 570)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
